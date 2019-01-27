b140ac1abc879d928ba5c8b2feae134942f7d84b
Oyekan Gbare <oyekanorisunmigbare@gmail.com>
2019-01-27 21:40:33 +0100

    completed page navigation

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,14 +15,17 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 // import mix-it-up
 import mixitup from 'mixitup' 
+// smoothScroll
+import smoothScroll from 'smoothscroll'
+// waypoints
 
 import  logo from './images/car.png';
 import workIcon from './images/work-icon.png'
-import { AST_Null } from 'terser';
+
 // /import testThumbnail from './images/project-thumbails/mai-ajiva.jpg'
 class HomePage extends Component{
     componentDidMount(){ 
-        // condigure slideout
+        // condigure slideout 
         const slideout = new Slideout({
             'panel': document.getElementById('panel'),
             'menu': document.getElementById('menu'),
@@ -37,11 +40,20 @@ class HomePage extends Component{
         const rellax =new Rellax('.rellax')
         // configure typed
         const typed = new Typed('#typed', {
-            strings: ['studio^500-republik!^5000','sAtisfAction?','time-keeping?', 'consistency?','creatiVity?'],
+            strings: ['studio^500-republik!^5000'],
             smartBackspace: true,
             typeSpeed: 70,
             backSpeed:20,
             backDelay: 300,
+            loop: true,
+            startDelay: 0,
+        })
+        const typedFooter = new Typed('#footer-typed', {
+            strings: ['Developed', 'Designed', 'Created'],
+            smartBackspace: true,
+            typeSpeed: 70,
+            backSpeed:20,
+            backDelay: 1000,
             loop: true,
             startDelay: 0,
         })
@@ -58,16 +70,40 @@ class HomePage extends Component{
                 navBar.classList.remove('nav-active')
             }
         })
-        // anime animation
-        // anime({
-        //     targets: '.st0',
-        //     strokeDashoffset: [anime.setDashoffset, 0],
-        //     easing: 'easeInOutSine',
-        //     duration: 1500,
-        //     delay: function(el, i) { return i * 250 },
-        //     direction: 'alternate',
-        //     loop: true
-        //   });
+        
+
+        // add class to nav element on scroll to the referring section
+        function navActiveToggler(section){ //determine if an element is in view
+            var navLinks = document.querySelectorAll('.nav-link')
+            var referringNavName = section.getAttribute('data-navreferrer')
+            var referringNav = document.querySelectorAll(`[data-scrollto=${referringNavName}]`)
+            navLinks.forEach((navLink)=>{
+                if(navLink.getAttribute('data-scrollto') === referringNavName) {
+                    navLink.classList.add('active')
+                }else{
+                    navLink.classList.remove('active')
+                }
+               
+            })
+        }
+        // 
+        var sections = document.querySelectorAll('.scrollSection') 
+        // check if an element is in viewport
+        var isInViewport = function (elem) {
+            var bounding = elem.getBoundingClientRect();
+            return (
+                bounding.top >= 0 &&
+                bounding.left >= 0 &&
+                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        };
+       window.addEventListener('scroll',()=>{ //add active class to corresponding nav if in view
+           sections.forEach((section)=>{
+               if(isInViewport(section)) navActiveToggler(section)
+           })
+       })
+       // end of this
     }
 render(){
     return(
@@ -102,19 +138,19 @@ class TopNav extends Component{
                    <div className='nav-container'>
                    <ul className="nav header-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#home">Home</a>
+                            <a className="nav-link active scroll-link"  data-scrollto ='home' href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#work">Our Work</a>
+                            <a className="nav-link scroll-link"  data-scrollto ='work' href="#work">Our Work</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About/Contact</a>
+                            <a className="nav-link scroll-link" data-scrollto ='about' href="#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#services">Our Services</a>
+                            <a className="nav-link scroll-link"  data-scrollto ='services' href="#services">Our Services</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
+                            <a className="nav-link scroll-link" data-scrollto ='contact' href="#contact">Contact</a>
                         </li>
                     </ul>
                    </div>
@@ -136,19 +172,19 @@ render(){
              </div>
             <ul className="nav side-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#home">Home</a>
+                            <a className="nav-link  active" data-scrollto ='home' href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#work">Our Work</a>
+                            <a className="nav-link" data-scrollto ='work' href="#work">Our Work</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About/Contact</a>
+                            <a className="nav-link" data-scrollto ='about'  href="#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#services">Our Services</a>
+                            <a className="nav-link"  data-scrollto ='services' href="#services">Our Services</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
+                            <a className="nav-link" data-scrollto ='contact' href="#contact">Contact</a>
                         </li>
             </ul>
         </nav>
@@ -160,21 +196,21 @@ class LandingParralax extends Component{
     render(){
         return(
             <>
-            <div id='landing-parralax' > 
+            <div className='landing-parralax scrollSection' data-navreferrer='home' id='home'> 
                <div className='parralax-content-container' >
-                   <p className='intro-top-text'> VISUAL MARKETING <span className='top-text-divider'>|</span> BRAND MARKETING <span className='top-text-divider'>|</span> CONSULTING</p>
-                    <div className='parralax-content rellax' data-rellax-speed="5">
+                   <p className='intro-top-text'> Video <span className='top-text-divider'>|</span> Graphics <span className='top-text-divider'>|</span> Hub</p>
+                    <div className='parralax-content rellax' data-rellax-speed="10">
                             <span className='intro-text-heading'>
                             <span id='typed'></span>
                             </span>
                             <span className='intro-text'>
                               We got you covered.
                             </span>
-                            <span className='intro-button'>
+                            <a className='intro-button' href='#work'>
                             Explore
-                            </span>
+                            </a>
                     </div>
-                    <p className='intro-top-text'><span className='top-text-divider'>&copy;</span> 2019</p>
+                    <a className='intro-top-text' href='te1:0803432324'><span className='top-text-divider fas fa-phone'></span> 080-733-443-3</a>
                </div>
              </div>
             </>
@@ -240,7 +276,7 @@ _showModal= (bool,url)=>{
     render(){
         return(
             <>
-            <div className='work-section'>
+            <div className='work-section scrollSection' data-navreferrer='work' id='work'>
                 <div className='section-title col-md-8 col-md-offset-2 text-center'
                 data-aos="fade-in"
                 data-aos-offset="100"
@@ -305,7 +341,7 @@ _showModal= (bool,url)=>{
 class AboutSection extends Component{
     render(){
         return(
-            <div className='about-parralax'>
+            <div className='about-parralax scrollSection'  data-navreferrer='about' id='about'>
                 <div className='parralax-content-container'>
                         <div className='about-section-header'>
                             <img src={logo} class='section-logo'/>
@@ -318,7 +354,7 @@ class AboutSection extends Component{
 
                         </div>
                         <div className='about-section-footer'>
-                        <a href='#our-work'> See Our Works</a>
+                        <a href='#services'> See Our Services</a>
                         </div>
                 </div>
             </div>
@@ -330,15 +366,21 @@ class AboutSection extends Component{
 class ContactFooter extends Component{
  render(){
         return(
-            <div className='contact-footer'>
+            <div className='contact-footer scrollSection' data-navreferrer='contact' id='contact'>
                 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="10 15 690 133">
                 <text transform="matrix(0.99 0 0 1 22.6216 112)" class="st0 st1 st2 st3">STUDIO REPUBLIK</text>
                 </svg>
                 <div className='contact-buttons'>
                <div> <a href='#contact-form' class='contact-form-button'>  Open Contact Form</a> </div>
-               <div> <a href="tel:+(234)9044343453" class='tel-num'> 090-4433-434e</a> </div>
+               <div> <span className='fas fa-phone contact-icon'></span><a href="tel:+(234)9044343453" class='tel-num'> 090-4433-434e</a> </div>
+               <div> <span className='fas fa-envelope contact-icon'></span><a href="mail:studiorepublik@yahoo.com" class='tel-num'>studio-republik@yahoo.com</a> </div>
+               <div className='social-icons-div'> <span className='fab fa-instagram'></span> <span className='fab fa-twitter'></span> <span className='fab fa-facebook'></span> </div>
                  </div>
+                 <footer> <a><span id='footer-typed'></span> by Abrand &copy;{new Date().getFullYear()}</a></footer>
             </div>
+            
+            
+           
         )
     }
 }
@@ -385,7 +427,7 @@ class Modal extends Component{
 class Services extends Component{
 render(){
     return(
-        <div className='service-cont'>
+        <div className='service-cont scrollSection' data-navreferrer='services' id='services'>
             <div className='work-section'>
                 <div className='section-title col-md-8 col-md-offset-2 text-center'
                 data-aos="fade-up"
